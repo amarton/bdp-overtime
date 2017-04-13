87c90e8c700fb5f8b2b0c52abb95cd2ce664107c
Adam Marton <adam@adammarton.com>
2017-04-13 13:01:31 -0400

    updated 2017 number

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@ var dataproject = {
 	},
 	share: function(){
 		$(".icon-twitter").on("click", function(){
-			var tweet = ""; //Tweet text
-			var url = ""; //Interactive URL
+			var tweet = "Graphic: Baltimore Police Department Overtime Expenditures 2012 - 2017"; //Tweet text
+			var url = "http://data.baltimoresun.com/graphics/bdp-overtime/"; //Interactive URL
 			var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
 			window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
 		});
 		$(".icon-facebook").on("click", function(){
 			var picture = ""; //Picture URL
-			var title = ""; //Post title
-			var description = ""; //Post description
-			var url = ""; //Interactive URL
+			var title = "Graphic: BPD Overtime"; //Post title
+			var description = "Baltimore Police Department Overtime Expenditures 2012 - 2017"; //Post description
+			var url = "http://data.baltimoresun.com/graphics/bdp-overtime/"; //Interactive URL
 	    	var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";    		
 			window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
 		});
@@ -21,5 +21,6 @@ var dataproject = {
 }
 $(document).ready(function(){
 	dataproject.init();
+	dataproject.share();
 	console.log("connected");
 });
